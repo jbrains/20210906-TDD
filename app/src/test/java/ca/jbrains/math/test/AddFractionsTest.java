8a5b6360e10f4d0b8e7e3d2c300be18da66cc871
J. B. Rainsberger <me@jbrains.ca>
2021-09-07 08:59:04 -0300

    Made tests look more uniform, in order to make them easier to read.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 public class AddFractionsTest {
     @Test
     void zeroPlusZero() {
-        Fraction sum = new Fraction(0).plus(new Fraction(0));
-        Assertions.assertEquals(0, sum.intValue());
+        Assertions.assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
     }
 
     @Test
