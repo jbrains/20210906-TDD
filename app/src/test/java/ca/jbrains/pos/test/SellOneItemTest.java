43a2b941fbf8c06de0d28fd2932b1cf5e6a77273
J. B. Rainsberger <me@jbrains.ca>
2021-09-08 10:13:17 -0300

    We can now display any line of text we need from within the Sale.

--- app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,27 +1,52 @@
 package ca.jbrains.pos.test;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SellOneItemTest {
     @Test
     void productFound() {
-        Sale sale = new Sale();
         Display display = new Display();
+        Sale sale = new Sale(display);
 
         sale.onBarcodeScanned("12345");
 
         Assertions.assertEquals("EUR 7.95", display.getText());
     }
 
+    @Test
+    @Disabled("refactoring")
+    void anotherProductFound() {
+        Display display = new Display();
+        Sale sale = new Sale(display);
+
+        sale.onBarcodeScanned("23456");
+
+        Assertions.assertEquals("EUR 12.50", display.getText());
+    }
+
     public static class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
         public void onBarcodeScanned(String barcode) {
+            display.setText("EUR 7.95");
         }
     }
 
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "EUR 7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
