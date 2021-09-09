e99b09a144b931f65e9cbb171f1e3420a1b416b2
J. B. Rainsberger <me@jbrains.ca>
2021-09-09 11:39:37 -0300

    The Controller can handle selling one item, as long as the Catalog finds a product for the scanned barcode.

---
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -0,0 +1,44 @@
+package ca.jbrains.pos.test;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class SellOneItemControllerTest {
+    @Test
+    void productFound() {
+        Catalog catalog = Mockito.mock(Catalog.class);
+        Display display = Mockito.mock(Display.class);
+
+        String scannedBarcode = "12345";
+        Price matchingPrice = Price.euroCents(795);
+
+        Mockito.when(catalog.findPrice(scannedBarcode)).thenReturn(matchingPrice);
+        
+        new SellOneItemController(display).onBarcodeScanned(scannedBarcode);
+        
+        Mockito.verify(display).displayPrice(matchingPrice);
+    }
+
+    public static class SellOneItemController {
+        private Display display;
+
+        public SellOneItemController(Display display) {
+            this.display = display;
+        }
+
+        public void onBarcodeScanned(String barcode) {
+            display.displayPrice(Price.euroCents(795));
+        }
+    }
+    public interface Display {
+        void displayPrice(Price price);
+    }
+    public interface Catalog {
+        Price findPrice(String barcode);
+    }
+    public record Price(int centsValue) {
+        public static Price euroCents(int centsValue) {
+            return new Price(centsValue);
+        }
+    }
+}
