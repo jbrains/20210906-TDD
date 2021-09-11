62795c074607f7271903b3e66e27e1532dce682e
J. B. Rainsberger <me@jbrains.ca>
2021-09-11 16:09:54 -0300

    We can now consume and dispatch any command, but only the first one.

--- app/src/test/java/ca/jbrains/pos/test/ConsumeAndDispatchCommandsTest.java
+++ app/src/test/java/ca/jbrains/pos/test/ConsumeAndDispatchCommandsTest.java
@@ -3,12 +3,14 @@ package ca.jbrains.pos.test;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
 public class ConsumeAndDispatchCommandsTest {
     @Test
-    void oneCommand() {
+    void oneCommand() throws Exception {
         CommandInterpreter commandInterpreter = Mockito.mock(CommandInterpreter.class);
 
         consumeAndDispatchCommands(new StringReader("::command 1::"), commandInterpreter);
@@ -16,8 +18,8 @@ public class ConsumeAndDispatchCommandsTest {
         Mockito.verify(commandInterpreter).handleCommand("::command 1::");
     }
 
-    private void consumeAndDispatchCommands(Reader commandReader, CommandInterpreter commandInterpreter) {
-        commandInterpreter.handleCommand("::command 1::");
+    private void consumeAndDispatchCommands(Reader commandReader, CommandInterpreter commandInterpreter) throws IOException {
+        commandInterpreter.handleCommand(new BufferedReader(commandReader).readLine());
     }
 
     public interface CommandInterpreter {
