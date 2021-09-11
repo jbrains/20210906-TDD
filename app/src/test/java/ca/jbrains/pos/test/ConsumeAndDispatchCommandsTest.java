e475b8736f5058caa71c1ad917c2892aa31a9243
J. B. Rainsberger <me@jbrains.ca>
2021-09-11 17:42:59 -0300

    We can now consume and dispatch several commands.

--- app/src/test/java/ca/jbrains/pos/test/ConsumeAndDispatchCommandsTest.java
+++ app/src/test/java/ca/jbrains/pos/test/ConsumeAndDispatchCommandsTest.java
@@ -1,12 +1,16 @@
 package ca.jbrains.pos.test;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.mockito.verification.VerificationMode;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.Arrays;
 
 public class ConsumeAndDispatchCommandsTest {
     @Test
@@ -27,10 +31,20 @@ public class ConsumeAndDispatchCommandsTest {
         Mockito.verifyNoInteractions(commandInterpreter);
     }
 
+    @Test
+    void severalCommands() throws Exception {
+        CommandInterpreter commandInterpreter = Mockito.mock(CommandInterpreter.class);
+
+        consumeAndDispatchCommands(new StringReader("::command 1::\n::command 2::\n::command 3::"), commandInterpreter);
+
+        InOrder inOrder = Mockito.inOrder(commandInterpreter);
+        Arrays.asList("::command 1::", "::command 2::", "::command 3::").stream().forEachOrdered(
+                each -> inOrder.verify(commandInterpreter).handleCommand(each)
+        );
+    }
+
     private void consumeAndDispatchCommands(Reader commandReader, CommandInterpreter commandInterpreter) throws IOException {
-        String commandText = new BufferedReader(commandReader).readLine();
-        if (commandText != null)
-            commandInterpreter.handleCommand(commandText);
+        new BufferedReader(commandReader).lines().forEachOrdered(commandInterpreter::handleCommand);
     }
 
     public interface CommandInterpreter {
