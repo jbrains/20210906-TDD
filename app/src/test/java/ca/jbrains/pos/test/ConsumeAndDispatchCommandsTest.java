7d4f5842cfe005a4df3c7973e5c52fa9f81b9027
J. B. Rainsberger <me@jbrains.ca>
2021-09-13 11:34:38 -0300

    Improved a name.

--- app/src/test/java/ca/jbrains/pos/test/ConsumeAndDispatchCommandsTest.java
+++ app/src/test/java/ca/jbrains/pos/test/ConsumeAndDispatchCommandsTest.java
@@ -10,7 +10,6 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,7 +18,7 @@ public class ConsumeAndDispatchCommandsTest {
     void oneCommand() throws Exception {
         CommandInterpreter commandInterpreter = Mockito.mock(CommandInterpreter.class);
 
-        readLinesOfTextThenCanonicalizeThemIntoCommandsThenDispatchCommands(new StringReader("::command 1::"), commandInterpreter, this::canonicalizeLineIntoCommand);
+        runTerminal(new StringReader("::command 1::"), commandInterpreter, this::canonicalizeLineIntoCommand);
 
         Mockito.verify(commandInterpreter).handleCommand("::command 1::");
     }
@@ -28,7 +27,7 @@ public class ConsumeAndDispatchCommandsTest {
     void noCommands() throws Exception {
         CommandInterpreter commandInterpreter = Mockito.mock(CommandInterpreter.class);
 
-        readLinesOfTextThenCanonicalizeThemIntoCommandsThenDispatchCommands(new StringReader(""), commandInterpreter, this::canonicalizeLineIntoCommand);
+        runTerminal(new StringReader(""), commandInterpreter, this::canonicalizeLineIntoCommand);
 
         Mockito.verifyNoInteractions(commandInterpreter);
     }
@@ -37,7 +36,7 @@ public class ConsumeAndDispatchCommandsTest {
     void severalCommands() throws Exception {
         CommandInterpreter commandInterpreter = Mockito.mock(CommandInterpreter.class);
 
-        readLinesOfTextThenCanonicalizeThemIntoCommandsThenDispatchCommands(new StringReader("::command 1::\n::command 2::\n::command 3::"), commandInterpreter, this::canonicalizeLineIntoCommand);
+        runTerminal(new StringReader("::command 1::\n::command 2::\n::command 3::"), commandInterpreter, this::canonicalizeLineIntoCommand);
 
         InOrder inOrder = Mockito.inOrder(commandInterpreter);
         Arrays.asList("::command 1::", "::command 2::", "::command 3::").stream().forEachOrdered(
@@ -76,7 +75,7 @@ public class ConsumeAndDispatchCommandsTest {
         Assertions.assertEquals(expectedCommands, canonicalCommands.collect(Collectors.toList()));
     }
 
-    private void readLinesOfTextThenCanonicalizeThemIntoCommandsThenDispatchCommands(Reader commandReader, CommandInterpreter commandInterpreter, CanoncalizeCommand canoncalizeCommand) {
+    private void runTerminal(Reader commandReader, CommandInterpreter commandInterpreter, CanoncalizeCommand canoncalizeCommand) {
         readLinesOfText(commandReader)
                 .map(canoncalizeCommand::canonicalizeCommand)
                 .forEachOrdered(commandInterpreter::handleCommand);
